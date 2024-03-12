cc1803e340333474e1657dd7ddb5340e643bfa28
shaheen2013 - Imtiaz <coredeveloper.2013@gmail.com>
2024-03-12 21:25:13 +0600

    category model design

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Product List.
+     *
+     * @OA\Get(
+     *     path="/api/category",
+     *     tags={"Category"},
+     *     security={{ "apiAuth": {} }},
+     *
+     *     @OA\MediaType(mediaType="multipart/form-data"),
+     *
+     *     @OA\Parameter(
+     *          in="query",
+     *          name="pagination",
+     *          required=true,
+     *
+     *          @OA\Schema(type="number"),
+     *          example="10"
+     *      ),
+     *
+     *      @OA\Parameter(
+     *          in="query",
+     *          name="get_all",
+     *          required=false,
+     *
+     *          @OA\Schema(type="boolean")
+     *      ),
+     *     @OA\Response(
+     *           response=200,
+     *           description="success",
+     *
+     *           @OA\JsonContent(
+     *
+     *               @OA\Property(property="data", type="json", example={}),
+     *               @OA\Property(property="links", type="json", example={}),
+     *               @OA\Property(property="meta", type="json", example={}),
+     *           )
+     *       ),
+     *
+     *       @OA\Response(
+     *           response=401,
+     *           description="Invalid user",
+     *
+     *           @OA\JsonContent(
+     *
+     *               @OA\Property(property="success", type="boolean", example="false"),
+     *               @OA\Property(property="errors", type="json", example={"message": {"Unauthenticated"}}),
+     *           )
+     *       )
+     * )
+     */
+    public function index()
+    {
+        $categories = Category::paginate(10);
+        return response()->json(['success' => true, 'data' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'description' => 'required'
+        ]);
+
+        if ($validateData->fails()) {
+            return response()->json(['success' => false, 'errors' => $validateData->errors()], 422);
+        }else{
+            Category::create($request->only('name', 'description'));
+            return response()->json(['success' => true, 'message' => 'Category created successfully.']);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
