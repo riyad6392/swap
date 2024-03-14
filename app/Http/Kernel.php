a7c614b7bae8e071a7ecbc84bc17be27a692eb77
shaheen2013 - khaled <coredeveloper.2013@gmail.com>
2024-03-14 16:42:09 +0600

    added passport scopes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.subscription' => \App\Http\Middleware\checkSubscription::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
     ];
 }
