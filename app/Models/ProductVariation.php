c17ddd9888cade6cbf354b0d74d3ae7765ff0ac2
shaheen2013 - khaled <coredeveloper.2013@gmail.com>
2024-03-15 16:31:36 +0600

    added fillable property in model

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariation extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'size',
+        'color',
+        'price',
+        'stock',
+        'discount',
+        'quantity',
+        'discount_type',
+        'discount_start_date',
+        'discount_end_date'
+    ];
+
+    public function image() {
+        return $this->belongsTo(Image::class);
+    }
 }
