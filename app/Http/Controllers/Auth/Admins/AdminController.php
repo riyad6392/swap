034191b1c0ef22508b76701da9af2060aebb4b23
shaheen2013 - khaled <coredeveloper.2013@gmail.com>
2024-03-29 16:31:26 +0600

    write swagger doc for user approved

--- app/Http/Controllers/Auth/Admins/AdminController.php
+++ app/Http/Controllers/Auth/Admins/AdminController.php
@@ -8,6 +8,31 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+
+    /**
+     * User Approved by Admin.
+     * @OA\Post (
+     *     path="/api/admin/approve-user/{user}",
+     *     tags={"Admin Authentication"},
+     *     security={{ "apiAuth": {} }},
+     *      @OA\Response(
+     *          response=200,
+     *          description="success",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="success", type="boolean", example="true"),
+     *              @OA\Property(property="message", type="string", example="User approved by admin!"),
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Invalid user",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="success", type="boolean", example="false"),
+     *              @OA\Property(property="errors", type="json", example={"message": {"Unauthenticated"}}),
+     *          )
+     *      )
+     * )
+     */
     public function approveUser(User $user)
     {
         if (!$user) {
