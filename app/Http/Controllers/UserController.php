22897d30ae65ee8203ca41a988c3784a6b2ed2d9
kiurd-gentleman <k.r.imtiaz@gmail.com>
2024-04-22 20:23:45 +0600

    implement user profile update method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\User\ListUserRequest;
 use App\Models\User;
+use App\Services\FileUploadService;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -60,18 +61,28 @@ class UserController extends Controller
      *       )
      * )
      */
-    public function index(Request $request)
+    public function index(ListUserRequest $listUserRequest)
     {
-        $users = User::query();
+        $users = User::query()->with('receivedRatings');
 
-        if ($request->get('get_all')) {
+        if ($listUserRequest->has('search')) {
+
+            $users->where('name', 'like', '%' . request('search') . '%');
+        }
+
+        if ($listUserRequest->has('sort')){
+
+            $users->orderBy('name', $listUserRequest->sort);
+        }
+
+        if ($listUserRequest->get('get_all')) {
 
             return response()->json(['success' => true, 'data' => $users->get()]);
         }
 
-        $categories = $users->paginate($request->pagination ?? self::PER_PAGE);
+        $users = $users->paginate($listUserRequest->pagination ?? self::PER_PAGE);
 
-        return response()->json(['success' => true, 'data' => $categories]);
+        return response()->json(['success' => true, 'data' => $users]);
     }
 
     /**
@@ -93,9 +104,21 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Request $request, string $id)
     {
-        //
+        $user = User::with('receivedRatings')->find($id);
+
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found'], 404);
+        }
+
+        if ($request->get('get_all')) {
+            return response()->json(['success' => true, 'data' => $user->products()->get()]);
+        }
+
+        $inventory = $user->products()->paginate($request->pagination ?? self::PER_PAGE);
+
+        return response()->json(['success' => true, 'data' => $inventory]);
     }
 
     /**
@@ -191,7 +214,8 @@ class UserController extends Controller
      * )
      */
     public function userList(ListUserRequest $listUserRequest){
-        $users = User::query();
+
+        $users = User::query()->with('image');
 
         if ($listUserRequest->has('search')) {
 
@@ -208,9 +232,9 @@ class UserController extends Controller
             return response()->json(['success' => true, 'data' => $users->get()]);
         }
 
-        $categories = $users->paginate($listUserRequest->pagination ?? self::PER_PAGE);
+        $users = $users->paginate($listUserRequest->pagination ?? self::PER_PAGE);
 
-        return response()->json(['success' => true, 'data' => $categories]);
+        return response()->json(['success' => true, 'data' => $users]);
     }
 
     /**
@@ -278,5 +302,18 @@ class UserController extends Controller
         $inventory = $user->products()->paginate($request->pagination ?? self::PER_PAGE);
 
         return response()->json(['success' => true, 'data' => $inventory]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $user = User::find(auth()->id());
+
+        if ($request->has('image')) {
+            FileUploadService::uploadFile($request->image, $user, 'image');
+        }
+
+        $user->update($request->all());
+
+        return response()->json(['success' => true, 'data' => $user]);
     }
 }
