2638463d422aa48f4dd3d4f5cbe813e22ad60e86
kiurd-gentelman <65729806+kiurd-gentleman@users.noreply.github.com>
2024-04-24 23:40:31 +0600

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
-            $table->string('subscription_is_active')->default(0);
+            $table->boolean('subscription_is_active')->default(0);
             $table->string('stripe_customer_id')->nullable();
             $table->boolean('is_approved_by_admin')->default(0);
             $table->boolean('is_super_swapper')->default(0);
