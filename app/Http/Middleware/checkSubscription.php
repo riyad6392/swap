476846ef1f3902788156d594ded3fb82866d7f2d
kiurd-gentleman <k.r.imtiaz@gmail.com>
2024-04-25 14:02:25 +0600

    subscription middleware update

--- app/Http/Middleware/checkSubscription.php
+++ app/Http/Middleware/checkSubscription.php
@@ -17,13 +17,28 @@ class checkSubscription
     {
         if (auth()->guard('api')->check()) {
             $user = auth()->guard('api')->user();
-            if (!$user->subscription_is_active) {
+            $userSubscriptions = $user->subscriptions()->where('status', 'active')->get();
+
+            if ($userSubscriptions->isEmpty()) {
+                $user->subscription_is_active = 0;
                 return response()->json([
                     'success' => false,
                     'message' => 'You are not subscribed to any plan.'
                 ], 403);
             }
+            elseif ( $userSubscriptions->isNotEmpty() && $userSubscriptions->last()->end_date < now()) {
+                $user->subscription_is_active = 0;
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Your subscription has expired.'
+                ], 403);
+            }
+            else {
+                $user->subscription_is_active = 1;
+                return $next($request);
+            }
         }
+
         return $next($request);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\HasDatabaseNotifications;
 use Illuminate\Notifications\Notifiable;
@@ -113,5 +114,10 @@ class User extends Authenticatable
     public function image()
     {
         return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function subscriptions(): HasOne
+    {
+        return $this->hasOne(Subscription::class)->where('status', 'active');
     }
 }
