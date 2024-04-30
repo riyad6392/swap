7648aaac2270adfefedf7fc63a416185da27838b
kiurd-gentleman <k.r.imtiaz@gmail.com>
2024-04-30 14:16:07 +0600

    overwrite the unauthenticated method in the  Authenticate middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+
 
 class Authenticate extends Middleware
 {
@@ -13,5 +15,10 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    protected function unauthenticated($request, array $guards)
+    {
+        abort(response()->json(['success' => false, 'message' => 'Unauthenticated.'], 401));
     }
 }
